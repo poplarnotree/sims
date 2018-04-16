4ca8431039658012d2226eee550838bf2ba664f2
poplarnotree <546837635@qq.com>
2018-04-17 02:07:12 +0800

    登出功能 + 数据库部门表，教师信息表建立

--- src/main/java/org/ycm/sims/controller/RoleController.java
+++ src/main/java/org/ycm/sims/controller/RoleController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.ycm.sims.dto.LoginDTO;
 import org.ycm.sims.service.RoleService;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
+ * 角色模块
  * Create by yangchangmin
  * 2018/4/14 23:17
  */
@@ -16,18 +19,37 @@ import org.ycm.sims.service.RoleService;
 public class RoleController {
 
     @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
     private RoleService roleService;
 
+    /**
+     * 登录页面
+     * @return
+     */
     @RequestMapping("/login")
     public String login(){
         return "/role/login";
     }
 
+    /**
+     * 验证登录
+     * @param loginName
+     * @param loginPassword
+     * @return
+     */
     @RequestMapping("/checkLogin")
     @ResponseBody
     public LoginDTO checkLogin(String loginName, String loginPassword){
 
         return roleService.login(loginName, loginPassword);
+    }
+
+    @RequestMapping("/logout")
+    public String logout(){
+        request.getSession().removeAttribute("loginName");
+        return "/role/login";
     }
 
 }
--- src/main/resources/sql/student.sql
+++ src/main/resources/sql/student.sql
@@ -14,3 +14,41 @@ INSERT INTO
   role(login_name, login_password, create_id, user_type)
 VALUES
   ('admin','123456',1000,0)
+
+-- teacher_information表
+CREATE TABLE teacher_information(
+  id INT NOT NULL AUTO_INCREMENT COMMENT '信息id',
+  login_name VARCHAR(20) NOT NULL UNIQUE comment '登录名',
+  number int NOT NULL comment '工号',
+  name VARCHAR(10) NOT NULL comment '姓名',
+  sex VARCHAR(2) NOT NULL comment '性别',
+  id_cart VARCHAR(18) NOT NULL comment '身份证号',
+  positional_titles VARCHAR(12) NOT NULL comment '职称',
+  nation VARCHAR (20) NOT NULL comment '民族',
+  place VARCHAR(20) NOT NULL comment '籍贯',
+  address VARCHAR(128) NOT NULL comment '地址',
+  birthday VARCHAR(10) NOT NULL comment '生日',
+  phone VARCHAR (11) NOT NULL comment '电话',
+  department VARCHAR (15) NOT NULL comment '部门',
+  classes VARCHAR (50) NOT NULL comment '班级',
+  subject VARCHAR (4) NOT NULL comment '学科',
+  create_time timestamp not null default current_timestamp comment '创建时间',
+  PRIMARY KEY (id),
+  FOREIGN KEY (login_name) REFERENCES role(login_name)
+)ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='教师信息表';
+
+-- department表
+CREATE TABLE department(
+  id INT NOT auto_increment NULL comment '部门id',
+  name VARCHAR (10) NOT NULL comment '部门名称',
+  create_time timestamp not null default current_timestamp comment '创建时间',
+  PRIMARY KEY (id)
+)ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='部门表';
+
+-- 插入3个部门表数据，学生处，教研组，学生部
+INSERT INTO
+  department(name)
+VALUES
+  ('学生处'),
+  ('教研组'),
+  ('学生部')
